85d74c579c2fc4df70f1749b83daf0a9fe97262e
EriikGabriel <erikgabriel.lins@hotmail.com>
2023-07-24 23:33:46 -0300

    chore: configure shadcn ui library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@providers/ThemeProvider"
 import "@styles/globals.css"
 
 import type { Metadata } from "next"
@@ -16,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
